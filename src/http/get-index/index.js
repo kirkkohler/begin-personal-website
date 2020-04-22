24852be7ef2ce6618193ff7e7ede4dda6de0ff05
Kirk Kohler <kirkkohler@users.noreply.github.com>
2020-04-22 00:40:04 -0700

    remove photography credit

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -23,10 +23,12 @@ exports.handler = async function Index () {
     /**
      * Layout
      */
+    /*
     photographer: 'Ivana Cajina',
     service: 'Unsplash',
     credit: 'https://unsplash.com/@von_co',
-    //image: staticAssetsHelper('background.jpg')
+    image: staticAssetsHelper('background.jpg')
+    */
     image: 'https://github.com/kirkkohler/kirkkohlerWebsite/blob/master/dist/images/bigSur2012-Large.jpg?raw=true'
     // or link to an external image URL such as ↓
     // image: 'https://images.unsplash.com/photo-1506535772317-9fdb71c959c6'
